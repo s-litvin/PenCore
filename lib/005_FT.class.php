76a69f7e1711767c73441323023654f134a0b8b8
s_litvin <litvin.xx@gmail.com>
2015-03-30 02:30:21 +0300

    FT::prepDate()

--- lib/005_FT.class.php
+++ lib/005_FT.class.php
@@ -10,4 +10,36 @@
 	{
 		return ROOTH_PATH . $str;
 	}
+	public static function prepDate($timestamp, $format = 'mysql')
+	{
+		switch ($format) {
+			case 'standard':
+				$format = 'F j, Y, g:i a'; // March 10, 2001, 5:16 pm
+				break;
+			case 'graph': $format = 'd M. D';        // 10 Mar. 2001
+				break;
+			case 'date': $format = 'd.m.Y';         // 10.03.2001
+				break;
+			case 'date_mysql': $format = 'Y-m-d';         // 2001-03-10
+				break;
+			case 'time': $format = 'H:i:s';         // 17:16:18
+				break;
+			case 'mysql': $format = 'Y-m-d H:i:s';   // 2001-03-10 17:16:18 (формат MySQL DATETIME)
+				break;
+			case 'jp': $format = 'Y/m/d H:i:s';   // 2001/03/10 17:16:18
+				break;
+			case 'eu': $format = 'd.m.Y H:i:s';   // 10.03.2001 17:16:18
+				break;
+			case 'usa': $format = 'm/d/Y H:i:s';   // 3/10/2001 17:16:18 (USA)
+				break;
+			case 'full': $format = 'D M j G:i:s T Y'; // Sat Mar 10 17:16:18 MST 2001
+				break;
+			case 'day_number': $format = 'jS';              // 10th
+				break;
+			case 'monolite': $format = 'YmdHis';          // 20010310171618
+				break;
+			default: $format = 'Y-m-d H:i';       // 2001-03-10 17:16
+		}
+		return date($format, $timestamp);
+	}
 }
