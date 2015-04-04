894172b620d7af108e38d43dca520803190c6a75
s_litvin <litvin.xx@gmail.com>
2015-04-05 02:47:41 +0300

    ф

--- APLICATION/app/views/404/404.html.php
+++ APLICATION/app/views/404/404.html.php
@@ -1,4 +1,4 @@
-<div class="large-12 text-center"> 
+<div class="large-12 text-center">
 	<img src="<?php echo FT::prepUrl('public/images/404.png');?>"/>
 	<h5>404 Страница не существует</h5>
 </div>
